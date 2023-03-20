ece36c0c26e9aee971151a957909a278e6c4d69b
Dean <dean@dpb.pw>
2023-03-19 17:49:08 -0700

    Changes to connection.js

--- db/connection.js
+++ db/connection.js
@@ -4,20 +4,16 @@ require('dotenv').config();
 
 // Creates the connection to database
 const connection = mysql.createConnection({
-  process.evv.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306,
-  }
+  host: "localhost",
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+},
 console.log(`Connected to the employees database.`)
-});
+);
 
 connection.connect(function (err) {
     if (err) throw err;
   });
   
 module.exports = connection;
-  
