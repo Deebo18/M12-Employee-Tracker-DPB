cef5536479a0755be5e0ab1bd96a60ad7ed023ba
Dean <dean@dpb.pw>
2023-04-09 08:03:39 -0700

    Seeds Update

--- db/seeds.sql
+++ db/seeds.sql
@@ -14,10 +14,9 @@ VALUES  ('Sales Lead', 100000, 1),
         ('Account Manager', 160000, 3),
         ('Accountant', 125000, 3),
         ('Legal Team Lead', 250000, 4),
-        ('Lawyer', 190000, 4),
-        ('Sales Lead', 100000, 1);
+        ('Lawyer', 190000, 4);
 
-INSERT INTO employees (first_name, last_name, role_id, manager_id)
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES  ('John', 'Doe', 1, NULL),
         ('Mike', 'Chan', 2, 1),
         ('Ashley', 'Rodriguez', 3, NULL),
